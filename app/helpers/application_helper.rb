ab128873e29ebd1c62b30ff41990e47058875de1
Lucas Castro <castro.lucas@gmail.com>
2017-02-06 13:06:42 -0300

    Primeiro commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,33 @@
+module ApplicationHelper
+  def app_name
+    "APP_NAME"
+  end
+
+  def background
+    '#f9f9f9'
+  end
+
+  def logo
+    nil
+  end
+
+  def logo_width
+    nil
+  end
+
+  def logo_header
+    nil
+  end
+
+  def logo_header_width
+    '252px'
+  end
+
+  def background_header
+    'inherit'
+  end
+
+  def link_color
+    '#428bca' 
+  end
+end
