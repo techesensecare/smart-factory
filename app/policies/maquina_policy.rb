dd52cc66621e176616d58251351e537bdeb06b56
Lucas Castro <castro.lucas@gmail.com>
2017-05-23 00:12:41 -0300

    Ajuste na politica da maquina

--- app/policies/maquina_policy.rb
+++ app/policies/maquina_policy.rb
@@ -1,4 +1,25 @@
 class MaquinaPolicy < ApplicationPolicy
+
+  def index?
+    %w(administrador).include? @user.perfil
+  end
+
+  def show?
+    index?
+  end
+
+  def create?
+    index?
+  end
+
+  def update?
+    index?
+  end
+
+  def destroy?
+    index?
+  end
+
   def iniciar_setup?
     %w(administrador pcp operador).include? @user.perfil
   end
