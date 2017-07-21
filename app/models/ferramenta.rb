c0adc31202fd896615a737cbef6968ed54d9105c
Lucas Castro <castro.lucas@gmail.com>
2017-07-21 13:43:24 -0300

    Removendo opções pré-definidas do tipo de ferramenta

--- app/models/ferramenta.rb
+++ app/models/ferramenta.rb
@@ -3,8 +3,6 @@ class Ferramenta < ApplicationRecord
 
   extend Enumerize
 
-  enumerize :tipo, in: %w(tipo1 tipo2), default: :tipo1, scope: true
-
   validates :descricao, presence: true
 
   default_scope { order('descricao ASC') }
