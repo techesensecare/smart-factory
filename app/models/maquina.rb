0c4313dee32a9022a97a005c8f66a217d25cffdb
Lucas Castro <castro.lucas@gmail.com>
2017-07-21 09:06:29 -0300

    Adicionando medidas para evitar cadastrar a mesma máquina mais de uma vez. - Exibindo lista de máquinas já cadastradas ao cadastrar uma nova; - Adicionando validação para como tornar a descrição única;

--- app/models/maquina.rb
+++ app/models/maquina.rb
@@ -9,7 +9,7 @@ class Maquina < ApplicationRecord
   has_many :etapas
 
   has_and_belongs_to_many :celulas
-  validates :descricao, presence: true
+  validates :descricao, presence: true, uniqueness: true
 
   has_attached_file :foto
   do_not_validate_attachment_file_type :foto
