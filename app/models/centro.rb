574434e7431630d646ca3d6e0d3e52a187fe1fba
Lucas Castro <castro.lucas@gmail.com>
2017-07-21 11:39:50 -0300

    Ajustes no centro de custo - Renomeado par Centro de trabalho - Impedindo cadastro duplicado (mesma descrição) - Exibindo centros já cadastrados ao criar um novo

--- app/models/centro.rb
+++ app/models/centro.rb
@@ -1,5 +1,7 @@
 class Centro < ApplicationRecord
   include Codigo
 
+  validates :descricao, presence: true, uniqueness: true
+
   scope :with_query, -> (q) { where("descricao ilike '%' || ? || '%'", q) }
 end
