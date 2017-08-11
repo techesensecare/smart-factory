8c024f7284f6651944268746bee7107b02605a62
Lucas Castro <castro.lucas@gmail.com>
2017-08-11 10:24:04 -0300

    Adicionando validação em matéria prima

--- app/models/materia_prima.rb
+++ app/models/materia_prima.rb
@@ -2,6 +2,5 @@ class MateriaPrima < ApplicationRecord
   belongs_to :produto
   belongs_to :produto_usado, class_name: 'Produto'
 
-  def a 
-  end
+  validates :produto_id, :produto_usado_id, :quantidade, presence: true
 end
