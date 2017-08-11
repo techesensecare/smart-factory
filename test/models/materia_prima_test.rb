ed4a60912c569e4eee18b65309015b66a226cbec
Lucas Castro <castro.lucas@gmail.com>
2017-08-11 10:24:22 -0300

    Testes de validação da matéria prima

--- test/models/materia_prima_test.rb
+++ test/models/materia_prima_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class MateriaPrimaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "produto_usado deve ser obrigatório" do
+    m = MateriaPrima.new
+    assert !m.save
+    assert_equal ["não pode ficar em branco"], m.errors[:produto_usado_id]
+  end
+
+  test "produto deve ser obrigatório" do
+    m = MateriaPrima.new
+    assert !m.save
+    assert_equal ["não pode ficar em branco"], m.errors[:produto_id]
+  end
+
+  test "quantidade deve ser obrigatório" do
+    m = MateriaPrima.new
+    assert !m.save
+    assert_equal ["não pode ficar em branco"], m.errors[:quantidade]
+  end
 end
