ec39ddd9bd91ee2bf89081256d291e01d6923109
Lucas Castro <castro.lucas@gmail.com>
2017-08-11 11:57:37 -0300

    Adicionando anexos

--- app/models/ferramenta.rb
+++ app/models/ferramenta.rb
@@ -8,4 +8,7 @@ class Ferramenta < ApplicationRecord
   default_scope { order('descricao ASC') }
 
   scope :with_query, -> (q) { where("descricao ilike '%' || ? || '%'", q) }
+
+  has_many :anexos, as: :anexavel
+  accepts_nested_attributes_for :anexos, :allow_destroy => true
 end
