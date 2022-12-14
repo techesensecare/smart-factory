4de2b6442c3ab65da8f5c3aa2cad64628e2c630f
Winiwon1@Z <brunopetinati@hotmail.com>
2022-12-14 14:47:59 -0300

    config database, initializer, application adicionados, bem como estilização inicial

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module SmartFactory
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.time_zone = 'Buenos Aires'
+    config.time_zone = 'Brasilia'
     config.i18n.default_locale = 'pt-BR'
     config.generators do |g|
       g.stylesheets false
