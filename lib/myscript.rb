5e8659c8c845ec0c2ff9ede47943f992cae4eb10
Winiwon1@Z <brunopetinati@hotmail.com>
2023-03-05 22:47:35 -0300

    modificando script de registros

--- lib/myscript.rb
+++ lib/myscript.rb
@@ -19,36 +19,22 @@ class MyScript
 
   def self.run
     
-    if destroy.materia_prima = 'materia_prima'
+    if ProdutosMrTaste.first.materia_prima = 'materia_prima'
       ProdutosMrTaste.first.destroy
     end
 
 
     ProdutosMrTaste.all.each do |pm|
-
-      existing_record = Produto.find_by(descricao: pm.materia_prima)
-
-      if existing_record == nil
-        existing_record = Produto.find_by(codigo: pm.codigo_ordem_producao)
-      end
-
-      if existing_record != nil
-        existing_record.update(
-          composto: true,
-          mr_taste_mp: pm.id
-        )
-      else
-        b = Produto.new(
-          codigo: pm.codigo_ordem_producao,
-          descricao: "#{pm.materia_prima}, OP: #{pm.codigo_ordem_producao}",
-          unidade_medida: format_weight(pm.unidade_medida),
-          tipo: nil,
-          codigo_automatico: false,
-          composto: true,
-          mr_taste_mp: pm.id
-        )
-        b.save
-      end
+      b = Produto.new(
+        codigo: "#{pm.codigo_ordem_producao}, ID: #{pm.id}",
+        descricao: "#{pm.materia_prima}, OP: #{pm.codigo_ordem_producao}, ID: #{pm.id}",
+        unidade_medida: format_weight(pm.unidade_medida),
+        tipo: nil,
+        codigo_automatico: false,
+        composto: true,
+        mr_taste_mp: pm.id
+      )
+      b.save
     end
   end
 end
